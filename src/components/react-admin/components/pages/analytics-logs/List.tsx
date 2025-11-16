ebdacef19109e72a186629ba6bd95df08d897fac
Taxi_tabby <rkdmf0000@gmail.com>
2025-11-17 00:57:41 +0900

    feat: change firstVisitAt field to DateField for improved date handling in AnalyticsVisitorList

--- src/components/react-admin/components/pages/analytics-logs/List.tsx
+++ src/components/react-admin/components/pages/analytics-logs/List.tsx
@@ -13,7 +13,7 @@ export const AnalyticsLogsList = () => {
         <TextField key="id" source="id" label="ID" />,
         <TextField key="fingerprint" source="visitorFingerprint" label="핑거프린트" />,
         <TextField key="type" source="type" label="타입" />,
-        <DateField key="timestamp" source="timestamp" label="발생 시간" />,
+        <DateField key="timestamp" source="timestamp" label="발생 시간" showTime />,
       ]}
       defaultSort={{ field: 'timestamp', order: 'DESC' }}
       rowClick="show"
--- src/components/react-admin/components/pages/analytics-visitors/List.tsx
+++ src/components/react-admin/components/pages/analytics-visitors/List.tsx
@@ -14,7 +14,7 @@ export const AnalyticsVisitorList = () => {
         <TextField key="fingerprint" source="fingerprint" label="핑거프린트" />,
 
         <TextField key="ipaddress" source="ipAddress" label="아이피 주소" />,
-        <TextField key="firstVisitAt" source="firstVisitAt" label="첫 방문일" />,
+        <DateField key="firstVisitAt" source="firstVisitAt" label="첫 방문일" showTime />,
       ]}
       defaultSort={{ field: 'firstVisitAt', order: 'DESC' }}
       rowClick="show"
