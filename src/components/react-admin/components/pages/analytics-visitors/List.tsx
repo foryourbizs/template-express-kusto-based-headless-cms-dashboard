86718e356e2310f6c7743e8a000e66b76045233d
Taxi_tabby <rkdmf0000@gmail.com>
2025-11-17 00:56:47 +0900

    fix: correct field name and default sort for firstVisitAt in AnalyticsVisitorList

--- src/components/react-admin/components/pages/analytics-visitors/List.tsx
+++ src/components/react-admin/components/pages/analytics-visitors/List.tsx
@@ -14,9 +14,9 @@ export const AnalyticsVisitorList = () => {
         <TextField key="fingerprint" source="fingerprint" label="핑거프린트" />,
 
         <TextField key="ipaddress" source="ipAddress" label="아이피 주소" />,
-        <TextField key="firstvisitat" source="firstVisitAt" label="첫 방문일" />,
+        <TextField key="firstVisitAt" source="firstVisitAt" label="첫 방문일" />,
       ]}
-      defaultSort={{ field: 'firstvisitat', order: 'DESC' }}
+      defaultSort={{ field: 'firstVisitAt', order: 'DESC' }}
       rowClick="show"
       actions={false}
       perPage={50}
