ce7e933e39b0fe6cb6c9799d3398f7797c944d7f
Taxi_tabby <rkdmf0000@gmail.com>
2025-11-17 00:42:25 +0900

    feat: add FilesList component for displaying analytics visitors with customizable columns and sorting

---
+++ src/components/react-admin/components/pages/analytics-logs/List.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { TextField, DateField, NumberField } from 'react-admin';
+import { GenericList } from '../../guesser/GenericList';
+
+export const FilesList = () => {
+  return (
+    <GenericList
+      queryOptions={{
+
+      }}
+      columns={[
+        <TextField key="id" source="id" label="ID" />,
+        <TextField key="fingerprint" source="visitorFingerprint" label="핑거프린트" />,
+        <TextField key="type" source="type" label="타입" />,
+        <DateField key="timestamp" source="timestamp" label="발생 시간" />,
+      ]}
+      defaultSort={{ field: 'timestamp', order: 'DESC' }}
+      rowClick="show"
+      perPage={50}
+    />
+  );
+};
---
+++ src/components/react-admin/components/pages/analytics-visitors/List.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { TextField, DateField, NumberField } from 'react-admin';
+import { GenericList } from '../../guesser/GenericList';
+
+export const FilesList = () => {
+  return (
+    <GenericList
+      queryOptions={{
+
+      }}
+      columns={[
+        <TextField key="id" source="id" label="ID" />,
+        <TextField key="fingerprint" source="fingerprint" label="핑거프린트" />,
+
+        <TextField key="ipaddress" source="ipAddress" label="아이피 주소" />,
+        <TextField key="firstvisitat" source="firstVisitAt" label="첫 방문일" />,
+      ]}
+      defaultSort={{ field: 'firstvisitat', order: 'DESC' }}
+      rowClick="show"
+      perPage={50}
+    />
+  );
+};
